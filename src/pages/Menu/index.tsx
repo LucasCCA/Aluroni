6bd0be4a704cd55fd7decfd0a6ed8323266a3332
Lucas <lucasamarale@hotmail.com>
2022-12-19 12:55:48 -0300

    Aula 03 - Header e Buscador

--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -1,12 +1,23 @@
 import styles from "./Menu.module.scss";
 import { ReactComponent as Logo } from "assets/logo.svg";
+import Searcher from "./Searcher";
+import { useState } from "react";
 
 export default function Menu() {
+  const [search, setSearch] = useState("");
+
   return (
     <main>
       <nav className={styles.menu}>
         <Logo />
       </nav>
+      <header className={styles.header}>
+        <div className={styles.header__text}>A casa do código e da massa</div>
+      </header>
+      <section className={styles.cardapio}>
+        <h3 className={styles.cardapio__titulo}>Cardápio</h3>
+        <Searcher search={search} setSearch={setSearch} />
+      </section>
     </main>
   );
 }
